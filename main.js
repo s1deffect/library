38e7ec1afc3fca9392ab43a4301c478e362e0041
unknown <holyhera0@gmail.com>
2023-03-03 11:07:27 +0200

    adding files

---
+++ main.js
@@ -0,0 +1,62 @@
+// // function player(name, marker) {
+// //     this.name = name,
+// //     this.marker = marker,
+// //     this.sayName = () => {
+// //         return console.log("hi" + this.name)
+// //     }
+// // }
+
+// // const playerOne = new player('ahmed', "x");
+// // console.log(playerOne.sayName())
+// // console.log(player.prototype)
+// let myLibraary = [];
+
+// function Book(title, author, pages, statue) {
+//     this.title = title,
+//     this.author = author,
+//     this.pages = pages,
+//     this.statue = statue
+//     this.info = () => {
+//         return console.log(`${this.title} by ${this.author} , ${this.pages}pages, ${this.statue}`)
+//     }
+// }
+
+
+// function addBook() {
+//     myLibraary.push();
+// }
+
+
+// const book1 = new Book('the hobit', 'martin', 300, "not read yet")
+// console.log(book1)
+// console.log(book1.info())
+
+
+
+
+
+
+// function student(name, grade){
+//     this.name = name,
+//     this.grade = grade
+// }
+// console.log(student.prototype)
+// student.prototype.sayName = () => {
+//     return console.log(this.name)
+// }
+
+// student.prototype = Object.create(Array.prototype)
+
+
+// console.log(student.prototype.)
+// // console.log(Array.prototype)
+
+function personFactory(name, age) {
+    const sayHi = () => {console.log(`hi ${name}`)}
+    return {name, age, sayHi}
+}
+
+const person1 = personFactory("ahmed", 28)
+
+console.log(person1)
+person1.sayHi()
