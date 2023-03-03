26d2d5c16cbccd88da4aa0f50b5a6ad708954b26
unknown <holyhera0@gmail.com>
2023-03-03 12:58:02 +0200

    editing

--- main.js
+++ main.js
@@ -51,12 +51,48 @@
 // console.log(student.prototype.)
 // // console.log(Array.prototype)
 
-function personFactory(name, age) {
-    const sayHi = () => {console.log(`hi ${name}`)}
-    return {name, age, sayHi}
+// function personFactory(name, age) {
+//     const sayHi = () => {console.log(`hi ${name}`)}
+//     return {name, age, sayHi}
+// }
+
+// const person1 = personFactory("ahmed", 28)
+
+// console.log(person1)
+// person1.sayHi()
+
+let myLibraary = [];
+
+function Book(title,author, pages, statue) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.statue = statue;
+}
+const book1 = new Book("game of thrones", "martin luther", 101, "read")
+myLibraary.push(book1)
+console.log(myLibraary)
+
+
+
+function addBookToLibrary() {
+    myLibraary.push(this)
 }
 
-const person1 = personFactory("ahmed", 28)
+function creatCard(book) {
+    const card = document.createElement('div')
+    const title = document.createElement('h1')
+    title.textContent= this.title
+    const author = document.createElement('h2')
+    author.textContent = this.author
+    const pages = document.createElement('div')
+    pages.textContent = this.pages
 
-console.log(person1)
-person1.sayHi()
+}
+
+//display book on the page
+function displayBook() {
+    for (book in myLibraary) {  
+        creatCard(book);
+    }
+}
